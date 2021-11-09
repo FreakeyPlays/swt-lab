115795f89eecd86e8aa338e3796b20f7fb2e1c51
FreakeyPlays <chrismerck@gmx.de>
2021-11-09 17:20:32 +0100

    Conflict

--- timemanagement/src/main/java/de/hse/swt/timemanagement/mainController.java
+++ timemanagement/src/main/java/de/hse/swt/timemanagement/mainController.java
@@ -146,19 +146,11 @@ public class mainController implements Initializable {
         LocalDate currentMonth = LocalDate.now();
         LocalDate localeDate = wktDatePicker.getValue();
         selectedDate = localeDate;
-<<<<<<< HEAD
-        
-        //Query if selected month/year equal to current month/year
-        boolean monthQuery = currentMonth.getMonth() == localeDate.getMonth();
-        boolean yearQuery = currentMonth.getYear() == localeDate.getYear();
-        
-=======
 
-        // Query if month/year equal to current month/year
+        // Query if selected month/year equal to current month/year
         boolean monthQuery = currentMonth.getMonth() == selectedDate.getMonth();
         boolean yearQuery = currentMonth.getYear() == selectedDate.getYear();
 
->>>>>>> branch 'dev' of https://gitlab.hs-esslingen.de/chmeit02/swt-tms.git
         if (localeDate == null) {
             wktDateTxt.setText("No Date selected.");
             setTimes("00:00:00", "00:00:00");
