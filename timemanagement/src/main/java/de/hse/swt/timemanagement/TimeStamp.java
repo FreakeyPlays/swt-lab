0eb376bceff3b5975f3769ceb057c4a2f38e2660
FreakeyPlays <chrismerck@gmx.de>
2021-11-11 12:03:09 +0100

    minor Bug fix in the TimeStamp Class (added the package Path)

--- timemanagement/src/main/java/de/hse/swt/timemanagement/TimeStamp.java
+++ timemanagement/src/main/java/de/hse/swt/timemanagement/TimeStamp.java
@@ -1,23 +1,25 @@
+package de.hse.swt.timemanagement;
+
 import java.sql.Timestamp;
 import java.util.Date;
 
 public class TimeStamp {
-	
+
 	private Date date;
 	private Timestamp ts;
-	
+
 	public TimeStamp() {
 		initComponents();
 	}
-	
+
 	public Timestamp getTimestamp() {
-        ts=new Timestamp(date.getTime());  
-        System.out.println(ts);//Test Printout of the Timestamp
-        return ts;//Returntype based on the sql Timestamp!!
+		ts = new Timestamp(date.getTime());
+		System.out.println(ts);// Test Printout of the Timestamp
+		return ts;// Returntype based on the sql Timestamp!!
 	}
-	
+
 	private void initComponents() {
 		date = new Date();
 	}
-	
-}    
+
+}
