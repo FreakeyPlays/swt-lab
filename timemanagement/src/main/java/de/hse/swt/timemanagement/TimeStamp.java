0fcdacd39aef56745ea19e9f7e3373b4ba05c357
mastit20 <92151117+mastit20@users.noreply.github.com>
2021-11-17 00:17:39 +0100

    Finished with TimeStamp -tested and time gets displayed correct and is in the database correct

--- timemanagement/src/main/java/de/hse/swt/timemanagement/TimeStamp.java
+++ timemanagement/src/main/java/de/hse/swt/timemanagement/TimeStamp.java
@@ -40,8 +40,18 @@ public class TimeStamp {
 		finalHours = (finalTime / 60) / 60;
 		finalMinutes = (finalTime - ((finalHours * 60) * 60)) / 60;
 		finalSeconds = finalTime % 60;
+		
+		String sh = (""+finalHours);	//Dieser block ist dazu da dass uhrzeiten in dem format angegeben werden können : 10:03:23
+		if(finalHours < 10)			//Code, der die Variablen hours minute und seconds verändert über diesem block spätestens ausdefinieren!!!
+			sh = ("0"+finalHours);
+		String sm = (""+finalMinutes);
+		if(finalMinutes < 10)
+			sm = ("0"+finalMinutes);
+		String ss = (""+finalSeconds);
+		if(finalSeconds < 10)
+			ss = ("0"+finalSeconds);
 
-		return (finalHours + ":" + finalMinutes + ":" + finalSeconds);
+		return (sh + ":" + sm + ":" + ss);
 	}
 
 }
